2415386a325a5b92e7bf7db5bce040d299f368ae
mullersen <mariannhaugland@gmail.com>
2020-04-24 02:46:08 +0000

    bulma initialized, navbar set up

---
+++ MVP/resources/js/components/Root.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <h1>Thisis Root</h1>
+
+    </div>
+</template>
+
+<script>
+import Car from './Car.vue';
+import Van from './Van.vue';
+import Addons from './Addons.vue';
+import Cart from './Cart.vue';
+
+
+    export default {
+        name: "Root",
+        components: {
+            Car,
+            Van,
+            Addons,
+            Cart,
+        },
+        mounted(){
+
+        }
+    }
+</script>
